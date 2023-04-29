1015cfa8fcbbff52c723b54676db8ec80ae19f9b
oregonjoe <joe@chetcodigital.com>
2023-04-29 14:58:22 -0700

    stack test app

--- web/app.py
+++ web/app.py
@@ -1,3 +1,7 @@
+import urllib
+from urlparse import urlparse
+import psycopg2  
+
 #from flask import Flask
 from flask import (
   Flask,
@@ -12,8 +16,22 @@ from flask import (
   jsonify
 )
 
+
+
+
+
+
 #from flask.ext.cors import CORS, cross_origin
 from flask_cors import CORS, cross_origin
+
+
+
+from psycopg2.pool import ThreadedConnectionPool
+db_pool = ThreadedConnectionPool(
+  1, # min connections,
+  int(os.environ.get('MAX_DB_CONNECTIONS',3)),
+  **connection_from(os.environ['DATABASE_URL'])
+)
 
 
 
