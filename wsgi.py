9e628c5026b1d2683ed15381d13e407cb58eb091
oregonjoe <joe@chetcodigital.com>
2023-04-29 14:03:52 -0700

    stack test app

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 
-from app.main import app
- 
-if __name__ == "__main__":
+#from app.main import app
+from web.app import app 
+
+#if __name__ == "__main__":
+if __name__ == "__app__":
         app.run()
