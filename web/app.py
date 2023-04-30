7e76e9d660c6b42f7dda1ba7a40628ce7a81fa4d
oregonjoe <joe@chetcodigital.com>
2023-04-30 15:30:36 -0700

    stack test app

--- web/app.py
+++ web/app.py
@@ -391,6 +391,183 @@ def callback_handling():
     return redirect('/')
 
 
+@app.route('/dashboards')
+@cross_origin()
+def dashboards():
+
+
+
+    response = make_response(render_template('dashboards.html', features = []))
+    #response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    #response.headers['Cache-Control'] = 'public, no-cache, no-store, max-age=0'
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
+  
+
+@app.route('/dashboard')
+#@login_required
+@cross_origin()
+#@requires_auth
+def dashboard():
+
+
+    try:
+      
+      if session['profile'] is not None:
+        
+        try:
+          mydata = session['profile']
+          log.info("dashboard: customdata:%s", mydata)
+          
+        
+          if mydata is not None:
+            user_email = mydata['name']
+            log.info("dashboard.html: user exists:%s", user_email)
+           
+        except:
+          e = sys.exc_info()[0]
+          log.info('dashboard.html: Error in geting user.custom_data  %s:  ' % str(e))
+          return render_template('dashboards_list.html', user=session['profile'], env=env) 
+
+        try:
+          if user_email is not None:
+
+            conn = db_pool.getconn()
+            session['username'] = user_email
+            
+            log.info("dashboard.html: email:%s", user_email )
+
+            query = "select userid from user_devices where useremail = %s group by userid"
+            
+            cursor = conn.cursor()
+            cursor.execute(query, [user_email])
+            i = cursor.fetchone()       
+            if cursor.rowcount > 0:
+
+                session['userid'] = str(i[0])
+                #session['adminid'] = verificationdata['email']
+            else:
+                session['userid'] = hash_string('helmsmart@mockmyid.com')
+
+            # cursor.close
+            db_pool.putconn(conn)
+
+            log.info("dashboard.html: userid:%s", session['userid'])
+
+            response = make_response(render_template('dashboard.html', features = []))
+            response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate, post-check=0, pre-check=0, max-age=0'
+            response.headers['Pragma'] = 'no-cache'
+            response.headers['Expires'] = '-1'
+            return response
+  
+        except:
+          e = sys.exc_info()[0]
+          log.info('dashboard.html: Error in geting user_email  %s:  ' % str(e))
+          pass
+          
+    except:
+      e = sys.exc_info()[0]
+      log.info('dashboard.html: Error in geting user  %s:  ' % str(e))
+      pass
+
+
+    return render_template('dashboards_list.html', user=session['profile'], env=env) 
+
+@app.route('/freeboard_getdashboardlist')
+@cross_origin()
+def freeboard_getdashboardlist():
+
+  userid = request.args.get('userid',1)
+
+
+  dashboardlists = getdashboardlists(userid)
+  
+  log.info("freeboard_GetDashboardJSON prefuid %s -> %s", userid, dashboardlists)
+
+
+  return jsonify({'preferences':dashboardlists})
+  #  result = json.dumps(r, cls=DateEncoder)
+
+  #response = make_response(dashboardlists)
+  #response.headers['Cache-Control'] = 'public, max-age=0'
+  #response.headers['content-type'] = "application/json"
+  #return response
+
+
+
+
+@app.route('/help')
+@cross_origin()
+def help():
+
+    response = make_response(render_template('index.html', features = []))
+    #response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+
+
+
+def getdashboardlists(userid):
+
+
+    conn = db_pool.getconn()
+
+    log.info("freeboard getdashboardlists data Query %s", userid)
+
+    try:
+    # first check db to see if deviceapikey is matched to device id
+
+        cursor = conn.cursor()
+
+        cursor.execute("select prefuid, prefname  from dashboard_prefs where userid = %s" , (userid,))
+
+        #log.info("freeboard getdashboardlists response %s", cursor)            
+
+        # see we got any matches
+        if cursor.rowcount == 0:
+            return jsonify( message='Could not get prefuids', status='error')
+            db_pool.putconn(conn) 
+            return ""
+        
+        else:
+          preferences = [dict((cursor.description[i][0], value) \
+            for i, value in enumerate(row)) for row in cursor.fetchall()]
+
+          log.info("freeboard getdashboardlists response %s", preferences)     
+          db_pool.putconn(conn) 
+          return preferences
+
+
+    except TypeError as e:
+        log.info('freeboard: getdashboardlists TypeError in geting deviceid  %s:  ', userid)
+        log.info('freeboard: getdashboardlists TypeError in geting deviceid  %s:  ' % str(e))
+            
+    except KeyError as e:
+        log.info('freeboard: getdashboardlists KeyError in geting deviceid  %s:  ', userid)
+        log.info('freeboard: getdashboardlists KeyError in geting deviceid  %s:  ' % str(e))
+
+    except NameErro as e:
+        log.info('freeboard: getdashboardlists NameError in geting deviceid  %s:  ', userid)
+        log.info('freeboard: getdashboardlists NameError in geting deviceid  %s:  ' % str(e))
+            
+    except IndexError as e:
+        log.info('freeboard: getdashboardlists IndexError in geting deviceid  %s:  ', userid)
+        log.info('freeboard: getdashboardlists IndexError in geting deviceid  %s:  ' % str(e))  
+
+
+    except:
+        log.info('freeboard: getdashboardlists Error in geting  deviceid %s:  ', userid)
+        e = sys.exc_info()[0]
+        log.info('freeboard: getdashboardlists Error in geting deviceid  %s:  ' % str(e))
+
+    # cursor.close
+    db_pool.putconn(conn)                       
+
+    return ""  
+
 
 
 #app.run(debug=True)
