fb3a1e1a9433ed48fe589e81c4374a1b84bf603b
oregonjoe <joe@chetcodigital.com>
2023-04-30 19:35:43 -0700

    stack test app

--- web/app.py
+++ web/app.py
@@ -776,10 +776,11 @@ def freeboard_savedashboardjson():
     #mymessage = request.data
     #mymessage = request.args.get('data')
     #mymessage = json.loads(request.data)
+    mymessage = json.dumps(request.data)
     #response = requests.post(url, headers=header, json=data)
     #log.info('freeboard_savedashboardjson: data  %s:  ', mymessage)
     
-    mymessage  = request.get_json()
+    #mymessage  = request.get_json()
 
   
     log.info('freeboard_savedashboardjson: json   %s:  ', mymessage)
