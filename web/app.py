6ca6b351d7d276a86e579ecc9fe0b933ab0f77be
oregonjoe <joe@chetcodigital.com>
2023-04-30 17:46:56 -0700

    stack test app

--- web/app.py
+++ web/app.py
@@ -702,7 +702,7 @@ def getdashboardlists(userid):
             return ""
         
         else:
-            log.info("freeboard getdashboardlists got matches %s : %s", cursor.description[0][0], value)
+            #log.info("freeboard getdashboardlists got matches %s : %s", cursor.description[0][0], value)
             preferences = [dict((cursor.description[i][0], value) \
                 for i, value in enumerate(row)) for row in cursor.fetchall()]
 
