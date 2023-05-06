4fd34e713dedd18f4e5e6557cffe3f8a544839ea
oregonjoe <joe@chetcodigital.com>
2023-05-06 12:00:56 -0700

    add new content

---
+++ web/static/js/MapSmartGlobals.js
@@ -0,0 +1,42 @@
+
+//***************************************************
+// Account Global Defaults
+//***************************************************
+var AccountDataFolder = "000000000000";
+var AccountType = 1;
+var ConfigOptions = 1;
+
+//***************************************************
+//  Page Parameters Global Defaults
+//***************************************************
+var PageID = 0;
+var CookiePrefix = "GSC1";
+var PageLayout = 0;
+var DialStart = 0;
+var PageBackGroundColor = "#336699";
+var PageForeGroundColor = "#04CCF0";
+var ClockLCDColor = 0;
+var SystemClockPGNID = 2000;
+var UpdateInterval = 5000;
+var DialTimeout = 720;
+var xmlDescription = "SeaDream 010214 Speed";
+ 
+//***************************************************
+// Dial Parameters Global Defaults
+//***************************************************
+ 
+var DialType = new Array(1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0);
+var DialPGNInstance = new Array(0,0,0,0,0,0,0,0);
+var DialPGNNumber = new Array("127488","127488","127489","127489","127489","127489","127489","127489");
+var DialPGNParameter = new Array(0,1,2,4,0,1,0,0);
+var DialUnits = new Array(24,8,0,18,8,0,0,0);
+var DialMin = new Array(0,0,0,0,0,0,0,0);
+var DialMax = new Array(6000,50,250,40,100,250,250,18);
+var DialAlarm = new Array(6000,45,200,20,10,200,200,10);
+var DialAlarmState = new Array(0,0,0,0,0,0,0,0);
+var DialText = new Array("Tachometer","Boost","Eng Temp","Fuel Rate","Oil Pressure","Oil Temp","Tran Temp","Alternator"); 
+var DialFrameType = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+var DialBackground = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+var DialLCDColor = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+var DialPointerColor = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+var DialPonterType = new Array(6,6,6,6,6,6,0,0,0,0,0,0,0,0,0,0); 
