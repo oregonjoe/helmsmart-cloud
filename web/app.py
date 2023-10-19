c29631e65b815ae59740fd82e57308dd2f63631b
oregonjoe <joe@chetcodigital.com>
2023-10-19 12:03:35 -0700

    pushsmart intergration test

--- web/app.py
+++ web/app.py
@@ -18755,7 +18755,7 @@ def events_endpoint(device_id, partition):
       dict(
               device_id = device_id,
               partition = partition,
-              payload =  request.data
+              payload =  request.data,
               content_type = request.content_type,
               dyno_id = os.environ['DYNO']
             ),
