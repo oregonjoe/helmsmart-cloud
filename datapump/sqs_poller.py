f4139975273760d1343ab86a82d4b57167eb1759
oregonjoe <joe@chetcodigital.com>
2023-10-23 11:41:45 -0700

    add sqs queues

--- datapump/sqs_poller.py
+++ datapump/sqs_poller.py
@@ -126,7 +126,8 @@ def proc(message):
         #records = nmea.loads((json.dumps(message_payload)))
         #records = nmea.loads((json.dumps(message_payload).decode("utf-8")))
         #records = nmea.loads(message_payload.decode("utf-8"))
-        records = nmea.loads(json.loads(message_payload))
+        #records = nmea.loads(json.loads(message_payload))
+        records = nmea.loads(str(message_payload))
         log.info('s3_poller Got SQS records %s: ', records)
 
         mysortedrecords = sorted(records, key=lambda t:t[1])
