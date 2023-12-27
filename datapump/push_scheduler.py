66630ecfdf2e157e154cf19155abf25c948b9bbf
oregonjoe <joe@chetcodigital.com>
2023-12-27 11:54:35 -0800

    sqs poller updates

--- datapump/push_scheduler.py
+++ datapump/push_scheduler.py
@@ -124,23 +124,23 @@ def put_SQS_Message(message_json):
 
         #print(response['MessageId'])
 
-        log.info("Send SQS:device_id %s:  response %s: ", device_id,response['MessageId'])
+        log.info("Send SQS:MessageBody %s:  response %s: ", MessageBody,response['MessageId'])
 
     except botocore.exceptions.ClientError as e:
-        log.info("Send SQS:ClientError device_id %s:  ", device_id)
+        log.info("Send SQS:ClientError MessageBody %s:  ", device_id)
         log.info('Send SQS:ClientError  Error in que SQS %s:  ' % e)
 
     except botocore.exceptions.ParamValidationError as e:
-        log.info("Send SQS:ParamValidationError device_id %s:  ", device_id)
+        log.info("Send SQS:ParamValidationError MessageBody %s:  ", MessageBody)
         log.info('Send SQS:ParamValidationError  Error in que SQS %s:  ' % e)
 
     except NameError as e:
-        log.info("Send SQS:NameError device_id %s:  ", device_id)
+        log.info("Send SQS:NameError MessageBody %s:  ", MessageBody)
         log.info('Send SQS:NameError  Error in que SQS %s:  ' % e)    
 
     except:
         e = sys.exc_info()[0]
-        log.info("Send SQS:device_id %s:  ", device_id)
+        log.info("Send SQS:MessageBody %s:  ", MessageBody)
         log.info('Send SQS: Error in que SQS %s:  ' % e)
 
 
