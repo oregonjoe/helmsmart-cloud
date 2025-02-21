e05041563d0f7516f4e62f80dc386e3dbb53f068
oregonjoe <joe@chetcodigital.com>
2025-02-21 10:15:54 -0800

    influxcloud insert test

--- datapump/sync.py
+++ datapump/sync.py
@@ -676,7 +676,7 @@ def convert_influxdb_cloud_tcpjson(value,  key):
     #Example KEY
     #key = 'deviceid:{}.sensor:tcp.source:0.instance:0.type:pushsmart.parameter:raw.HelmSmart'.format(deviceid)
     tagpairs = key.split(".")
-    if debug_all: log.info('freeboard: convert_influxdbcloud_json tagpairs %s:  ', tagpairs)
+    if debug_all_influxdb: log.info('freeboard: convert_influxdbcloud_json tagpairs %s:  ', tagpairs)
 
     myjsonkeys={}
 
@@ -690,7 +690,7 @@ def convert_influxdb_cloud_tcpjson(value,  key):
     #"deviceid:001EC010AD69.sensor:environmental_data.source:0.instance:0.type:Outside_Temperature.parameter:temperature.HelmSmart"
     #myjsonkeys = { 'deviceid':tag0[1], 'sensor':tag1[1], 'source':tag2[1], 'instance':tag3[1], 'type':tag4[1], 'parameter':tag5[1]}
     myjsonkeys = { 'deviceid':tag0[1], 'sensor':tag1[1], 'source':tag2[1], 'instance':tag3[1], 'type':PGN, 'parameter':'raw'}
-    if debug_all: log.info('freeboard: convert_influxdbcloud_json myjsonkeys %s:  ', myjsonkeys)
+    if debug_all_influxdb: log.info('freeboard: convert_influxdbcloud_json myjsonkeys %s:  ', myjsonkeys)
 
     #values = {'value':value}
     #values = {tag5[1]:value}
@@ -698,34 +698,34 @@ def convert_influxdb_cloud_tcpjson(value,  key):
     #ifluxjson ={"measurement":tagpairs[6], "time": ts, "tags":myjsonkeys, "fields": values}
     #ifluxjson ={"measurement":measurement, "time": ts, "tags":myjsonkeys, "fields": values}
     ifluxjson ={"measurement":measurement, "time":ps_ts,  'deviceid':tag0[1], 'source':tag2[1], "raw": value}
-    if debug_all: log.info('freeboard: convert_influxdbcloud_json %s:  ', ifluxjson)
+    if debug_all_influxdb: log.info('freeboard: convert_influxdbcloud_json %s:  ', ifluxjson)
 
 
     return ifluxjson
 
   except AttributeError as e:
-    if debug_all: log.info('Sync: AttributeError in convert_influxdb_cloud_tcpjson %s:  ', mytime)
+    if debug_all_influxdb: log.info('Sync: AttributeError in convert_influxdb_cloud_tcpjson %s:  ', mytime)
     #e = sys.exc_info()[0]
 
     if debug_all: log.info('Sync: AttributeError in convert_influxdb_cloud_tcpjson %s:  ' % str(e))
     
   except TypeError as e:
-    if debug_all: log.info('Sync: TypeError in convert_influxdb_cloud_tcpjson %s:  ', mytime)
+    if debug_all_influxdb: log.info('Sync: TypeError in convert_influxdb_cloud_tcpjson %s:  ', mytime)
     #e = sys.exc_info()[0]
 
-    if debug_all: log.info('Sync: TypeError in convert_influxdb_cloud_tcpjson %s:  ' % str(e))
+    if debug_all_influxdb: log.info('Sync: TypeError in convert_influxdb_cloud_tcpjson %s:  ' % str(e))
     
   except NameError as e:
     if debug_all: log.info('Sync: NameError in convert_influxdb_cloud_tcpjson %s:  ', mytime)
     #e = sys.exc_info()[0]
 
-    if debug_all: log.info('Sync: NameError in convert_influxdb_cloud_tcpjson %s:  ' % str(e))
+    if debug_all_influxdb: log.info('Sync: NameError in convert_influxdb_cloud_tcpjson %s:  ' % str(e))
     
   except:
-    if debug_all: log.info('Sync: Error convert_influxdb_cloud_tcpjson %s:', mytime)
+    if debug_all_influxdb: log.info('Sync: Error convert_influxdb_cloud_tcpjson %s:', mytime)
 
     e = sys.exc_info()[0]
-    if debug_all: log.info("Sync.py Error in convert_influxdb_cloud_tcpjson: %s" % e)
+    if debug_all_influxdb: log.info("Sync.py Error in convert_influxdb_cloud_tcpjson: %s" % e)
 
 
 #022025 JLB added influxdb Cloud insert test
