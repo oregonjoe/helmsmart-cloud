f16626947e4c13b74d5ad74ed20de639f2b83591
oregonjoe <joe@chetcodigital.com>
2025-02-21 15:54:53 -0800

    influxcloud insert test

--- web/app.py
+++ web/app.py
@@ -20209,7 +20209,7 @@ def freeboard_raw():
     #return jsonify(result="OK")
     log.info('freeboard:  InfluxDB-Cloud response  %s:', response)
 
-    values = response.get('psraw',[])
+    values = response.select(['psraw'])
     #keys = result.keys()
     log.info("freeboard Get InfluxDB psraw values %s", values)
 
