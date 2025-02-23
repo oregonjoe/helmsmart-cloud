7716bf2647bcc4a16b3ecb0a518d4c5e22dabb84
oregonjoe <joe@chetcodigital.com>
2025-02-23 13:24:26 -0800

    adding psraw_sqs_poller

--- web/app.py
+++ web/app.py
@@ -20259,7 +20259,9 @@ def freeboard_raw():
     #values = response.select(['psraw'])
     values = response.column('psraw')
     log.info("freeboard_raw Get InfluxDB psraw values %s", values)
+    log.info("freeboard_raw Get InfluxDB psraw json.dumps(values) %s", json.dumps(values))
 
+    
     nmea_records = nmea.loads((json.dumps(values)))
     log.info("freeboard_raw Get InfluxDB psraw nmea_records %s", nmea_records)
      
