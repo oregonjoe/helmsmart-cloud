2bc6a33f9befeb1a40e1f9d940e3edcea45226d8
oregonjoe <joe@chetcodigital.com>
2025-07-25 18:32:37 -0700

    adding seagaugeg4 configuration

--- web/app.py
+++ web/app.py
@@ -1899,10 +1899,15 @@ def seasmartconfig():
   #return jsonify(result="OK", postdata = postdata)
   #data_for_template = ["Feature A", "Feature B", "Feature C"]
   #response = make_response(render_template('seagauge_conf.html', features = []))
-  myjson={"ADCChannel":0, "CALFile":"VDO250F.xml"}
-  #myjson=jsonify(ADCChannel=0, CALFile="VDO250F.xml")
-  response = make_response(render_template('seagauge_conf.html', features = [myjson]))
-  #response = make_response(render_template('seagauge_conf.html?adcCalChannel=0&adcCalFile=VDo250.xml', features = []))
+
+  if adcCalChannel != "" and adcCalFile != "":
+    myjson={"ADCChannel":int(adcCalChannel), "CALFile":adcCalFile}
+    response = make_response(render_template('seagauge_conf.html', features = [myjson]))
+
+  else:
+    response = make_response(render_template('seagauge_conf.html'))
+
+
   #response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
   response.headers['Cache-Control'] = 'public, max-age=0'
   return response
