15801976d79d7b67a8ba0b1b0a43221b8775d7e8
oregonjoe <joe@chetcodigital.com>
2025-12-17 11:56:21 -0800

    aws cognito

--- web/app.py
+++ web/app.py
@@ -549,7 +549,7 @@ def manage():
   
   log.info('manage_details: response request.args %s:  ', request.args)
 
-  return jsonify({'access_token': request.args})
+  #return jsonify({'access_token': request.args})
   
   access_token = aws_auth.get_access_token(request.args)
   #claims = aws_auth.claims
