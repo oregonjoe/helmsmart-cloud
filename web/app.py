0b89c91440348a6df24b6b50dcbd87891f842b66
oregonjoe <joe@chetcodigital.com>
2026-01-05 12:52:30 -0800

    adding subscriptions

--- web/app.py
+++ web/app.py
@@ -1361,7 +1361,7 @@ def aws_alerts_get_admin_data():
 
   
   session['aws_clientid'] = environ.get("AWS_COGNITO_ADMIN_POOL_ID")
-  session['aws_domain'] = environ.get("AWS_COGNITO_DOMAIN")
+  session['aws_domain'] = environ.get("AWS_COGNITO_ADMIN_DOMAIN")
   #session['aws_access_token'] = token
   session['aws_access_token'] = ""
 
