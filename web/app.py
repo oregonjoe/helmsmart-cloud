b98e6fca543e3de2167723d4ac6010613e51c339
oregonjoe <joe@chetcodigital.com>
2026-01-05 13:13:50 -0800

    adding subscriptions

--- web/app.py
+++ web/app.py
@@ -1293,7 +1293,7 @@ def aws_alerts_get_user_data():
   session['aws_phone_verified'] = aws_phone_verified
 
   
-  session['aws_clientid'] = environ.get("AWS_COGNITO_USER_POOL_ID")
+  session['aws_clientid'] = environ.get("AWS_COGNITO_USER_POOL_CLIENT_ID")
   session['aws_domain'] = environ.get("AWS_COGNITO_DOMAIN")
   session['aws_access_token'] = access_token
 
