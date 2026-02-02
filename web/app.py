e437a1b0b4c8a9a1af26467761251e08f70e3626
oregonjoe <joe@chetcodigital.com>
2026-02-02 11:24:35 -0800

    adding authorize.net payments

--- web/app.py
+++ web/app.py
@@ -563,8 +563,11 @@ def get_payment_token():
   #transactionRequest.order.invoiceNumber = "678965432145"
   #transactionRequest.order.description = "SeaGAugeG4-4576"
   #transactionRequest.poNumber = "678965432145"
-
-
+  # Define transaction details (adjust as needed)
+  order = OrderType()
+  order.invoiceNumber = "678965432145"
+  order.description = "SeaGAugeG4-4576"
+  transactionRequest.order = order
 
   # 3. Request Hosted Form Token
   #request = apicontractsv1.getHostedPaymentPageRequest()
