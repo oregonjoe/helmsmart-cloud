06296fd96447cf93baf9620d930e6f694e84a4da
oregonjoe <joe@chetcodigital.com>
2026-02-03 17:48:53 -0800

    adding authorize.net payments

--- web/app.py
+++ web/app.py
@@ -835,7 +835,7 @@ def get_payment_token():
   setting2.settingName = "hostedPaymentReturnOptions"
   # The value must be a JSON string, which requires escaping quotes in Python
   #setting2.settingValue = "{\"showReceipt\": true}"
-  setting2.settingValue = "{\"showReceipt\": true, \"url\": \"https://www.helmsmart-cloud.com/manage\", \"urlText\": \"Continue\",\"cancelUrl\":\"https://www.helmsmart-cloud.com/manage\",\"cancelUrlText\":\"Cancel"}"
+  setting2.settingValue = "{\"showReceipt\": true, \"url\": \"https://www.helmsmart-cloud.com/manage\", \"urlText\": \"Continue\",\"cancelUrl\":\"https://www.helmsmart-cloud.com/manage\",\"cancelUrlText\":\"Cancel\"}"
   log.info('get_payment_token: setting2:  ')
 
   # 3. Customize the "Pay" button text
